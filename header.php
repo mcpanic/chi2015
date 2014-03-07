62295d686a540fa8b3273c04021814e828ee5fc8
Juho Kim <truepanic@gmail.com>
2014-03-07 16:57:57 -0500

    IE support, responsive separated, smaller logos

--- header.php
+++ header.php
@@ -13,7 +13,10 @@
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="css/main.css">
-
+        <!--[if lte IE 8]>
+            <link rel = "stylesheet" href="css/ie.css" />
+        <![endif]-->        
+        <!--[if !IE]><!--><link rel="stylesheet" href="css/responsive.css" /><!--<![endif]-->
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
     </head>
     <body>
