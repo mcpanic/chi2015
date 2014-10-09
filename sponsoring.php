521227f1e1454dff73a27e1644dfc739d2cd30f6
Juho Kim <truepanic@gmail.com>
2014-10-08 14:59:51 -1000

    update

--- sponsoring.php
+++ sponsoring.php
@@ -8,7 +8,7 @@
   <p>Your sponsorship of this premier conference is a strong statement about your organization's commitment to the field of HCI, as well as your commitment to the professional development of your staff.</p>
   <p>CHI 2015 continues to offer new and exciting opportunities for sponsors. Benefits this year include publicity and professional development as well as recruiting, networking and business development.</p>
   <p>For more detailed information about these and the many other advantages of sponsoring, please see the <a href="<?php echo $prefix; ?>/files/CHI2015_sponsor_invitation_Aug2014.pdf">CHI 2015 Invitation to Sponsor</a> or contact Carol Klyver, Sponsor and Exhibit Coordinator, at <a href="mailto:sponsors@chi2015.acm.org">sponsors@chi2015.acm.org</a>. Participation in the Sponsorship program is an ideal opportunity to demonstrate your organization's commitment to the field of HCI. CHI 2015 offers proven opportunities for your organization to make thousands of delegates aware of your commitment to high-quality interaction design.</p>
-  <p>See <a href="http://chi2015.acm.org/sponsoring/sponsors-of-chi2015">CHI 2015 sponsors</a>.</p>
+  <p>See the list of <a href="http://chi2015.acm.org/sponsoring/sponsors-of-chi2015">Sponsors of CHI 2015</a>.</p>
   <p>See the list of <a href="http://chi2014.acm.org/sponsorship/sponsors-of-chi2014">last year's sponsors</a>.</p>
 
   <h3>Related Links</h3>
