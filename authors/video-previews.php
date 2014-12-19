d3ab51bdab7315a4c8557be0a4ceb7c2098c992e
Juho Kim <truepanic@gmail.com>
2014-12-18 20:13:49 -0500

    vp

--- authors/video-previews.php
+++ authors/video-previews.php
@@ -7,16 +7,28 @@
 <h2>Video Previews</h2>
 </div>
 
-Content will be updated soon.
-<!--
 <div id="content-canvas">
-  <p>CHI 2013 introduced <strong>Video Previews</strong>, a new way to share research results with attendees and with members of the community who are not able to attend the conference. The goal was to help people plan their conference time, to help them discover interesting and important work, and to increase the visibility of presentations beyond the conference.</p>
-  <p>CHI 2015 will continue the use of video previews. All authors of accepted Papers and Notes, as well as some extended abstract venues, will be required to submit a 30 seconds video which describes their work. The video previews will be made available through different mediums (e.g., Youtube channel) before and during the conference to help attendees plan their daily schedules. They will also be archived and embedded on the abstract page of the ACM Digital Library.</p>
 
+<p>
+<strong>Video Previews</strong> are 30-second videos that can be thought of as an elevator pitch to give a glimpse of your work, and are here to help connect your work with the community at the conference and beyond. Since their introduction at CHI 2013, Video Previews have been adopted in world first class conferences such as ACM UIST and IEEE VIS. They provide the authors with a great opportunity to publicize their work to a wider audience and to attract more readers to their paper (*).
+</p>
+
+<p>
+CHI 2015 will continue the use of video previews. All authors of accepted Papers and Notes, as well as some extended abstract venues, are <strong>strongly encouraged</strong> to submit a 30 seconds video that describes their work. The Video Previews will be made available through different mediums before and during the conference to help attendees plan their daily schedules (ACM YouTube Channel, conference program application, digital proceedings). They will also be archived and embedded on the abstract page of the ACM Digital Library.
+</p>
+
+<p><i>
+If you have any questions, please contact the video preview chairs at <a href="mailto:videopreviews@chi2015.acm.org">videopreviews@chi2015.acm.org</a>.
+</i></p>
+
+<p>
+(*) CHI 2013 Video Previews have a record of 268,000 views since April 2013, and CHI 2014 Video Previews have about 119,000 views since April 2014 (aggregated data from the CHI website and the ACM YouTube Channel).
+</p>
 
   <h3>Deadline</h3>
-  <p>The deadline for submitting your video preview is <strong>February 16th 2015</strong>.</p>
-  <p>Video previews are required for the following venues.</p>
+  <p>Please carefully read the following instructions and submit your Video Preview in the dedicated "Video Preview" field of your final submission at <a href="http://www.precisionconference.com/~sigchi">http://www.precisionconference.com/~sigchi</a> by <strong>February 16th 2015, 17:00 PDT</strong>.</p>
+
+  <p>Video previews are strongly encouraged for the following venues:</p>
   <ul>
     <li>Papers and Notes</li>
     <li>Extended Abstracts
@@ -31,19 +43,39 @@ Content will be updated soon.
     </li>
     <li>TOCHI</li>
   </ul>
-  <p>The video previews will be released on April 15th, two weeks prior to the conference.</p>
+  <p>The <strong>Video Previews will be released on April 4th</strong>, two weeks prior to the conference.</p>
 
   <h3>Instructions</h3>
-  <p>Authors of accepted Papers &amp; Notes and Extended Abstracts (Panels, Case Studies, Courses, Interactivity, alt.CHI and SIGs) are required to create a video or other animated presentation to complete their final submission. The video material must conform to the format as described in the submission guide below. We strongly recommend you to read the <a href="video-previews-details"> detailed guidelines </a> which list a series of technical as well as production recommendations to help you create a compelling video preview of quality. For inspiration, please also visit the <a href="http://inputdesignoutput.com/previewsgallery-2014/gallery.html" target="_blank"> Video Previews Gallery</a> featuring examples from previous years.</p><p>
+<!--   <p>Authors of accepted Papers &amp; Notes and Extended Abstracts (Panels, Case Studies, Courses, Interactivity, alt.CHI and SIGs) are required to create a video or other animated presentation to complete their final submission. The video material must conform to the format as described in the submission guide below. We strongly recommend you to read the <a href="video-previews-details"> detailed guidelines </a> which list a series of technical as well as production recommendations to help you create a compelling video preview of quality. For inspiration, please also visit the <a href="http://inputdesignoutput.com/previewsgallery-2014/gallery.html" target="_blank"> Video Previews Gallery</a> featuring examples from previous years.</p> -->
 
-  </p><h3>Submission guide</h3>
-  <p>The following checklists have been designed to help you make sure that your material conforms to the Video Previews requirements. Please make sure that you go through <strong> every </strong> item of these lists. Detailed guidelines and advices are available by following the (i) icons.</p>
+    <ul>
+    <li>The first <strong>3 seconds</strong> of the video must include the <strong>title of your paper, authors list, affiliations and contact information</strong>.
+    </li>
+    <li>The rest of the video should communicate <strong>the topic and the message</strong> of your work (like an elevator pitch)
+    </li>
+    <li><strong>The video must be appropriate</strong> in terms of rights, use of language, viewpoints or imagery, and should not offend any individuals or groups at the conference or elsewhere (we reserve the right to reject videos whose content is inappropriate).
+    </li>
+    <li><strong>Subtitles</strong> should be included to improve accessibility to your video (recommended)
+    </li>
+    <li><strong>No advertisement for the conference talk</strong>. Video Previews will be archived on the abstract page of the ACM Digital Library as a short video figure to overview your work. Their diffusion therefore goes beyond the conference and should not be seen as a teaser to the presentation. Instead, Video Previews should be thought of as a video abstract to attract more readers to the paper.
+    </li>
+    </ul>
+
+<p>
+Your Video Preview material must conform to the format as described in the submission guide below. We strongly recommend you to read the <a href="http://chi2015.acm.org/authors/video-previews-details/">detailed guidelines</a>, which list a series of technical as well as production recommendations to help you create a compelling video preview of quality. For inspiration, you can watch the Video Previews of <a href="https://www.youtube.com/playlist?list=PLn0nrSd4xjjbHgNcNjfImwKjyb04gJzBs">CHI 2013</a> and <a href="https://www.youtube.com/playlist?list=PLn0nrSd4xjjaZ2cS7qsnBjgkvyAA4j79d">CHI 2014</a>.
+</p>
+
+  <h3>Submission guide</h3>
+    <p>The following checklists have been designed to help you make sure that your material conforms to the Video Previews requirements. Please make sure that you go through <strong>every</strong> item of these lists. Detailed guidelines and advices are available by following the (i) icons.</p>
+
+    <p>Before submitting, we also invite you to check the compliance of your file using the CHI Video Previews Checker (link coming soon).</p>
+
 
   <h4 id="technical-req">Technical requirements</h4>
   <table cellpadding="5px" cellspacing="0">
     <tbody><tr class="odd">
       <td> <input id="tr1" onclick="setCB(this)" type="checkbox"> </td>
-      <td> video file size must not exceed <strong>10 MB</strong> </td>
+      <td> video file size must not exceed <strong>20 MB</strong> </td>
       <td> </td>
     </tr>
     <tr class="even">
@@ -58,7 +90,7 @@ Content will be updated soon.
     </tr>
     <tr class="even">
       <td><input id="tr4" onclick="setCB(this)" type="checkbox"> </td>
-      <td>video resolution must be at <strong> 480p (854x480)</strong> </td>
+      <td>video resolution must be at <strong> 720p (1280x720)</strong> </td>
     </tr>
   </tbody></table>
 
@@ -71,7 +103,7 @@ Content will be updated soon.
     </tr>
     <tr class="even">
       <td> <input id="cr2" onclick="setCB(this)" type="checkbox"> </td>
-      <td> video must be <strong> captioned</strong> </td>
+      <td> video must be <strong> captioned</strong> (recommended)</td>
       <td> <a href="video-previews-details#subtitles"> <img src="/img/information.png" alt="Detailed instructions for subtitles and captions" title="Detailed instructions for subtitles and captions"></a> </td>
     </tr>
     <tr class="odd">
@@ -83,7 +115,7 @@ Content will be updated soon.
   <p><i> <strong>* Please note:</strong> Video Previews are subject to ACM's copyright policies. Please review <a href="http://www.acm.org/publications/policies/copyright_policy">ACM's Copyright and Permission Policy</a> and <a href="http://www.acm.org/publications/third-party-material">ACM's Requirements about 3rd party material.</a></i></p>
 </div>
 
-!-->
+
 
 <?php
   include "../footer.php";
