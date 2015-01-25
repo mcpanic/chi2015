3df72732b4a98e21d1ee1087e62b4d0ce8d5dd77
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2015-01-25 08:50:47 +0800

    Added schedule in courses using angular js
    All text data is in courses.js

--- header.php
+++ header.php
@@ -8,10 +8,10 @@ else {
 }
 ?>
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" ng-app="chi2015_app"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" ng-app="chi2015_app"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" ng-app="chi2015_app"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" ng-app="chi2015_app"> <!--<![endif]-->
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -48,6 +48,12 @@ else {
         <![endif]-->
         <!--[if !IE]><!--><link rel="stylesheet" href="<?php echo $prefix; ?>/css/responsive.css" /><!--<![endif]-->
         <script src="<?php echo $prefix; ?>/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+
+        <?php if (isset($ng_app)) :?>
+            <script src="<?php echo $prefix; ?>/js/angular/angular.min.js"></script>
+            <script src="<?php echo $prefix; ?>/js/angular/angular-sanitize.min.js"></script>
+            <script src="<?php echo $prefix; ?>/js/angular/modules/<?php echo $ng_app; ?>.js"></script>
+        <?php endif ?>
     </head>
     <body>
         <!--[if lt IE 7]>
