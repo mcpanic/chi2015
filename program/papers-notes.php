db8a327e91dcf6ba2c960be900e19cd1acb72ee0
Juho Kim <truepanic@gmail.com>
2015-01-27 12:01:50 -0500

    papers

---
+++ program/papers-notes.php
@@ -0,0 +1,36 @@
+<?php
+  $pageTitle = "Program | Papers and Notes";
+  include "../header.php";
+?>
+
+<div>
+    <h2>Program | Papers and Notes</h2>
+</div>
+
+<h3>Advance Program</h3>
+
+<p>
+A list of all the accepted CHI 2015 papers and notes is now available through Confer, a tool for conference attendees to discover papers of their interest, get paper recommendations, meet people with shared interests, and create their personal schedule.
+</p>
+
+<div class="link-button-wrapper">
+  <a class="link-button" href="http://confer.csail.mit.edu/login?redirect_url=/chi2015/schedule">
+    Open CHI 2015 Advance Program
+  </a>
+</div>
+
+<p>
+You can access the program across devices. You <a href="http://confer.csail.mit.edu/login?redirect_url=/chi2015/schedule">create an account to save your favorite papers and personal schedule</a>. Or, you can <a href="http://confer.csail.mit.edu/chi2015/schedule">browse the program as a guest</a>.
+</p>
+
+<p>
+Marking papers you're interested in will help you organize your schedule. But your activities will help the entire CHI community at the same time! Confer can improve its recommendation as more attendees specify interests. Also, conference organizers can better match sessions to rooms for an improved conference experience for everyone.
+</p>
+
+<p>
+The program will be updated as more gets finalized, so please check back. Initial session schedule will be announced in early February.
+</p>
+
+<?php
+  include "../footer.php";
+?>
