c0f6a58f55c71d74f51e5c98387a256bd9e145fd
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2015-01-27 22:01:27 +0800

    Added papers-notes.php placeholder

---
+++ program/papers-notes.php
@@ -0,0 +1,19 @@
+<?php
+  $pageTitle = "Program | Papers and Notes";
+  //$ng_app = "courses";
+
+  include "../header.php";
+?>
+
+<div>
+    <h2>Program | Papers and Notes</h2>
+</div>
+
+<div role="main" id="papers-notes">
+
+	<h3>Full list of Papers and Notes coming soon!!!</h3>
+
+</div>
+<?php
+  include "../footer.php";
+?>
