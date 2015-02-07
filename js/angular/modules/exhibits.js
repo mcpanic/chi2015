3b4c3ba7b268a781e3524f736939b2eaac3547fc
TJ <tjmonsi@gmail.com>
2015-02-07 14:31:23 +0800

    Changed CSS to make it look better and removed some unused code at controller

--- js/angular/modules/exhibits.js
+++ js/angular/modules/exhibits.js
@@ -3,14 +3,8 @@
  */
 
 angular.module('chi2015_controllers').controller("list_of_exhibitors_controller",
-  ["$scope", "$window", "exhibitors_factory",
-  function($scope, $window, exhibitors_factory) {
-
-    $scope.isWide = function() {
-      return $window.innerWidth > 768; //your breakpoint here.
-    }
-
-    angular.element($window).on('resize', angular.bind($scope, $scope.$apply));
+  ["$scope", "exhibitors_factory",
+  function($scope, exhibitors_factory) {
 
     $scope.data = []
 
