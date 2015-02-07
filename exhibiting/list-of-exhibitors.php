afd7920162879846fd80c1b2055601bc04cf4d9c
TJ <tjmonsi@gmail.com>
2015-02-07 14:18:21 +0800

    Added Exhibits

--- exhibiting/list-of-exhibitors.php
+++ exhibiting/list-of-exhibitors.php
@@ -27,31 +27,6 @@
 
 </div>
 
-<h3>Champion Sponsor Exhibitors</h3>
-
-<div class="row">
-  <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 exhibitor-header-row">Company/Product</div>
-  <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 exhibitor-header-row">Booth</div>
-  <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 exhibitor-header-row">Description</div>
-</div>
-
-<h3>Contributing Sponsor Exhibitors</h3>
-
-<div class="row">
-  <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 exhibitor-header-row">Company/Product</div>
-  <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 exhibitor-header-row">Booth</div>
-  <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 exhibitor-header-row">Description</div>
-</div>
-
-
-<h3>Additional Exhibitors</h3>
-
-<div class="row">
-  <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 exhibitor-header-row">Company/Product</div>
-  <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1 exhibitor-header-row">Booth</div>
-  <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 exhibitor-header-row">Description</div>
-</div>
-
 <?php
   include "../footer.php";
 ?>
