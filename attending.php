cfa2f80e162503a4871c66f6190f96d78a097096
Juho Kim <truepanic@gmail.com>
2015-03-12 15:16:46 -0400

    press

--- attending.php
+++ attending.php
@@ -18,10 +18,6 @@ CHI 2015 is being held for the first time in Asia in Seoul, Korea, a world-class
   <li><a href="#press-information">Press Information</a></li>
 </ul>
 
-<p>
-<img src="<?php echo $prefix; ?>/img/coex.jpg" width="100%">
-</p>
-
 <h3 id="industrial-and-city-tours">Industrial and City Tours</h3>
 There are two tour programs that are available. Please register here:
 <ul>
@@ -32,49 +28,14 @@ There are two tour programs that are available. Please register here:
     <a href="https://chi2015seoul.wordpress.com/2015/02/28/city-tour-program/">City Tour</a>
   </li>
 </ul>
-
-<h3 id="press-information">Press Information</h3>
 <p>
-  Thank you for your interest in reporting on the ACM CHI Conference on Human Factors in Computing Systems, CHI 2015. To get an overview of the conference, please take a look at the list of <a href="<?php echo $prefix; ?>/program/keynotes">Keynote Speakers</a> , the <a href="<?php echo $prefix; ?>/program/best-of-chi">Best of CHI</a> award papers and then the <a href="<?php echo $prefix; ?>/program/full-schedule">Full Conference Schedule</a>.
-</p>
-
-<h4>Registration</h4>
-<p>All members of the press must register for CHI 2015. To qualify for a CHI 2015 press pass, please complete the press accreditation form below.</p>
-<p>
-Please Note: <i>Even if you have qualified for a CHI press badge in the past, you must re-qualify for CHI 2015.</i>
-</p>
-
-<h4>CHI 2015 Press Accreditation Form</h4>
-<p>
-To provide you with the appropriate support, please send an e-mail with the information requested below to <a href="mailto:conferencechair@chi2015.acm.org?cc=chiregistration@executivevents.com&subject=Press%20Accreditation%20Form">Conference Chairs and Registrar</a>.
-</p>
-<ul>
-  <li>Name</li>
-  <li>Affiliation</li>
-  <li>Address</li>
-  <li>Web site/s with which you are associated</li>
-  <li>Phone number at which you can be reached</li>
-  <li>What is your area of expertise?</li>
-  <li>What are your areas of interest within the CHI 2015 program?</li>
-  <li>Are there particular presenters you would like to interview?  </li>
-  <li>When and where do you expect to publish your articles?</li>
-  <li>Please provide links to articles you have published.</li>
-  <li>Have you attended previous CHI conferences? If so, which ones?</li>
-  <li>Have you attended previous ACM conferences or events? If so, which ones?</li>
-  <li>Is there anything else we should know about your interest in covering CHI 2015?</li>
-</ul>
-<p>
-We will review your information and contact you within the next 48-72 hours.
-  </p>
-
-
-<br/>
-<p>
-  We look forward to seeing you in Seoul!!<br/><br/>
+  We look forward to seeing you in Seoul!!<br/>
   <a href="mailto:localhospitality@chi2015.acm.org">Local Hospitality Chair:</a><br>
     Junho Choi, Yonsei University, Korea
 </p>
-
+<p>
+<img src="<?php echo $prefix; ?>/img/coex.jpg" width="100%">
+</p>
 
 
 <?php
---
+++ attending/press.php
@@ -0,0 +1,42 @@
+<?php
+  $pageTitle = "Press";
+  include "../header.php";
+?>
+
+<div>
+    <h2>Attending | Press</h2>
+</div>
+
+<h3 id="press-information">Press Information</h3>
+<p>
+  Thank you for your interest in reporting on the ACM CHI Conference on Human Factors in Computing Systems, CHI 2015. To get an overview of the conference, please take a look at the list of <a href="<?php echo $prefix; ?>/program/keynotes">Keynote Speakers</a> , the <a href="<?php echo $prefix; ?>/program/best-of-chi">Best of CHI</a> award papers and then the <a href="<?php echo $prefix; ?>/program/full-schedule">Full Conference Schedule</a>.
+</p>
+
+<h4>Registration</h4>
+<p>All members of the press must register for CHI 2015. To qualify for a CHI 2015 press pass, please complete the press accreditation form below.</p>
+<p>
+Please Note: <i>Even if you have qualified for a CHI press badge in the past, you must re-qualify for CHI 2015.</i>
+</p>
+
+<h4>CHI 2015 Press Accreditation Form</h4>
+<p>
+To provide you with the appropriate support, please send an e-mail with the information requested below to <a href="mailto:conferencechair@chi2015.acm.org?cc=chiregistration@executivevents.com&subject=Press%20Accreditation%20Form">Conference Chairs and Registrar</a>.
+</p>
+<ul>
+  <li>Name</li>
+  <li>Affiliation</li>
+  <li>Address</li>
+  <li>Web site/s with which you are associated</li>
+  <li>Phone number at which you can be reached</li>
+  <li>What is your area of expertise?</li>
+  <li>What are your areas of interest within the CHI 2015 program?</li>
+  <li>Are there particular presenters you would like to interview?  </li>
+  <li>When and where do you expect to publish your articles?</li>
+  <li>Please provide links to articles you have published.</li>
+  <li>Have you attended previous CHI conferences? If so, which ones?</li>
+  <li>Have you attended previous ACM conferences or events? If so, which ones?</li>
+  <li>Is there anything else we should know about your interest in covering CHI 2015?</li>
+</ul>
+<p>
+We will review your information and contact you within the next 48-72 hours.
+  </p>
